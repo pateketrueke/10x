303f45672198c4d2a9b751302ba865fd860466ed
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-07-21 16:35:24 -0500

    Minimal setup for maths; WIP

---
+++ src/lib/solver.js
@@ -0,0 +1,157 @@
+import Convert from 'convert-units';
+
+import {
+  isExpr,
+} from './parser';
+
+export function parseFromValue(token) {
+  let text = token[1];
+
+  if (typeof text === 'number') {
+    text = text.toString().split(/(?=\d{2})/).join(':');
+    text += token[2] ? ` ${token[2]}` : '';
+  }
+
+  const now = new Date();
+  const year = now.getFullYear();
+  const today = now.toString().split(' ').slice(0, 4).join(' ');
+
+  if (text.includes(':')) return new Date(`${today} ${text}`);
+  if (hasMonths(text)) return new Date(!text.includes(',') ? `${text}, ${year}` : text);
+  if (text.toLowerCase() === 'yesterday') return (now.setDate(now.getDate() - 1), now);
+  if (text.toLowerCase() === 'tomorrow') return (now.setDate(now.getDate() + 1), now);
+  if (text.toLowerCase() === 'today') return new Date(`${today} 00:00`);
+  if (text.toLowerCase() === 'now') return now;
+
+  return text;
+}
+
+export function calculateFromDate(op, left, right) {
+  if (!(left instanceof Date)) {
+    const now = new Date();
+    const nowMonth = now.toString().split(' ')[1];
+    const nowDate = now.getDate();
+
+    // convert from given units
+    if (Array.isArray(left)) {
+      const secs = new Convert(left[1]).from(left[2]).to('s');
+
+      left = new Date();
+      left.setSeconds(secs);
+    }
+
+    // otherwise, just take the year
+    if (typeof left === 'number') left = new Date(`${nowMonth} ${nowDate}, ${left} 00:00`);
+  }
+
+  const oldYear = left.getFullYear();
+  const oldMonth = left.getMonth();
+  const oldDate = left.getDate();
+  const oldHours = left.getHours();
+  const oldMinutes = left.getMinutes();
+  const oldSeconds = left.getSeconds();
+
+  if (right instanceof Date) {
+    const newYear = right.getFullYear();
+    const newMonth = right.getMonth();
+    const newDate = right.getDate();
+    const newHours = right.getHours();
+    const newMinutes = right.getMinutes();
+    const newSeconds = right.getSeconds();
+
+    if (op !== 'at' && isExpr(op)) {
+      if (oldYear !== newYear) left.setYear(newYear);
+      if (oldMonth !== newMonth) left.setMonth(newMonth);
+    }
+
+    if (op === '-') {
+      if (oldYear !== newYear) left.setYear(oldYear - newYear);
+      if (oldMonth !== newMonth) left.setMonth(oldMonth - newMonth);
+      if (oldDate !== newDate) left.setDate(oldDate - newDate);
+      if (oldHours !== newHours) left.setHours(oldHours - newHours);
+      if (oldMinutes !== newMinutes) left.setMinutes(oldMinutes - newMinutes);
+      if (oldSeconds !== newSeconds) left.setSeconds(oldSeconds - newSeconds);
+    }
+
+    if (op === '+' || op === 'at') {
+      let isToday = true;
+
+      if (oldYear !== newYear) isToday = !left.setYear(oldYear + newYear);
+      if (oldMonth !== newMonth) isToday = !left.setMonth(oldMonth + newMonth);
+      if (oldDate !== newDate) isToday = !left.setDate(oldDate + newDate);
+
+      // operate only when both dates are not from same day!
+      if (!isToday) {
+        if (oldHours !== newHours) left.setHours(oldHours + newHours);
+        if (oldMinutes !== newMinutes) left.setMinutes(oldMinutes + newMinutes);
+        if (oldSeconds !== newSeconds) left.setSeconds(oldSeconds + newSeconds);
+      } else {
+        if (oldHours !== newHours) left.setHours(newHours);
+        if (oldMinutes !== newMinutes) left.setMinutes(newMinutes);
+        if (oldSeconds !== newSeconds) left.setSeconds(newSeconds);
+      }
+    }
+  } else {
+    if (op === '-') left.setSeconds(left.getSeconds() - right);
+    if (op === '+' || isExpr(op)) left.setSeconds(left.getSeconds() + right);
+  }
+
+  return left;
+}
+
+export function evaluateExpression(op, left, right) {
+  if (op === '+') return left + right;
+  if (op === '-') return left - right;
+  if (op === '*') return left * right;
+  if (op === '/') return left / right;
+}
+
+export function operateExpression(ops, expr) {
+  for (let i = 1, c = expr.length - 1; i < c; i += 1) {
+    const cur = expr[i];
+    const prev = expr[i - 1];
+    const next = expr[i + 1];
+
+    if (cur && ops.indexOf(cur[1]) > -1) {
+      let result;
+
+      console.log(prev, cur, next);
+
+      if (!isNaN(result)) {
+        const unit = expr.find(x => x[0] === 'unit');
+
+        expr.splice(i - 1, 3, ['number', result, unit ? unit[1] : prev[2]]);
+
+        // if tokens are left...
+        if (expr.length >= 3) {
+          return operateExpression(ops, expr);
+        }
+      } else {
+        throw new Error(`Invalid expression: ${prev[1]} ${cur[1]} ${next[1]}`);
+      }
+    }
+  }
+
+  return expr;
+}
+
+export function calculateFromTokens(expr) {
+  expr = operateExpression(['*', '/'], expr);
+  expr = operateExpression(['at', 'of', 'from', '+', '-', 'as', 'in'], expr);
+
+  return expr[0];
+}
+
+export function reduceFromAST(tokens) {
+  for (let i = 0, c = tokens.length; i < c; i += 1) {
+    const cur = tokens[i];
+
+    if (cur && Array.isArray(cur[0])) {
+      tokens[i] = calculateFromTokens(reduceFromAST(cur));
+    } else {
+      tokens[i] = cur;
+    }
+  }
+
+  return tokens;
+}
--- test.js
+++ test.js
@@ -1,23 +1,57 @@
 import transform from './src/lib/transform';
 
 import {
+  isOp,
+} from './src/lib/parser';
+
+import {
+  reduceFromAST,
+  calculateFromTokens,
+} from './src/lib/solver';
+
+import {
   convertFrom,
   DEFAULT_MAPPINGS,
 } from './src/lib/convert';
 
 const argv = process.argv.slice(Math.max(2, process.argv.indexOf('--') + 1));
 
-const tokens = transform(argv.join(' '), {
+const opts = {
   units: {
     ...DEFAULT_MAPPINGS,
-  },
-  values: {
-    'π': [['number', Math.PI]],
   },
   convert: (num, base, target) => {
     console.log('HANDLE', num, base, target);
     console.log('DEFAULTS', convertFrom(num, base, target));
   },
-});
+};
 
-console.log(tokens);
+const tokens = transform(argv.join(' '), opts);
+
+// OK, now we have tokens, and they're transformed into a validated sequence
+// next, is transform that into a tree... and finally, resolve everything inside
+// the thin is... how the AST should be parsed for, how defs/calls are handled by?
+
+// each time a unit is reached, we lookup for its value, if exists,
+// evaluate that chunk; otherwise probably the value is missing, and a simple conversion would work
+
+// maths are simple to solve as previously we did... but now, we need to understand function-calls and such,
+// so, the solver must care about that? I don't think so... it should care only of solved values, a reducer may?
+
+// yeah, the reduces would take those chunks and replace them in place, at the end we just need a new tree without fns/calls
+
+let lastOp = ['plus', '+'];
+
+// operate all possible expressions...
+const chunks = reduceFromAST(tokens.tree).reduce((prev, cur) => {
+  const lastValue = prev[prev.length - 1] || [];
+
+  if (lastValue[0] === 'number' && cur[0] === 'number') prev.push(lastOp, cur);
+  else prev.push(cur);
+
+  if (isOp(cur[1], '/*')) lastOp = cur;
+  return prev;
+}, []);
+
+console.log(require('util').inspect(chunks, { colors: true, depth: 5 }));
+console.log(require('util').inspect(calculateFromTokens(chunks), { colors: true, depth: 5 }));
