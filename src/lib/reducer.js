1e916cbacc6010a6aed8ce3bc814a1792613444f
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-07-22 02:57:51 -0500

    Fixed basic unit conversions

--- src/lib/reducer.js
+++ src/lib/reducer.js
@@ -112,9 +112,11 @@ export function reduceFromAST(tokens, convert, expressions = {}) {
       if (isTime(cur[2])) isDate = true;
 
       // handle converting between expressions
-      if (cur[0] === 'expr' && isExpr(cur[1]) && right[2] && left[2] !== right[2]) {
-        left[1] = convert(toNumber(left), left[2], right[2]);
-        left[2] = right[2];
+      if (cur[0] === 'expr' && isExpr(cur[1])) {
+        const fixedUnit = right[0] === 'unit' ? right[1] : right[2];
+
+        left[1] = convert(toNumber(left), left[2], fixedUnit);
+        left[2] = fixedUnit;
         tokens.splice(i, 2);
       }
 
