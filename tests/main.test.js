19855eb67562a2b6ffe064d45d88b535e6a45fe6
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-08-02 21:53:13 -0500

    Add tests; cleanup

---
+++ tests/main.test.js
@@ -0,0 +1,33 @@
+import Solvente from '../src/lib';
+import { deepEqual } from 'assert';
+
+const cases = [
+  ['# Expressions', []],
+  ['Expressions like 1 + 2 just works, however expressions like 1 2 3 / 4 5 are calculated differently.', ['3', '8.75']],
+  ['> Operator-less expressions between numbers are implicitly added/subtracted as above.', []],
+  ['In such cases, last unary-operator (+ or -) is still used on further operations, e.g. 1 2 3 - 4 5', ['-3']],
+  ['> Numbers can have some separators as $1,000 (or 1_000), add spaces to disambiguate.', []],
+  ['Notice: all math-expressions are disabled within formatting tags.', []],
+  ['# Definitions', []],
+  ['Local definitions are possible too, e.g. x=1.5; 3x', ['4.5']],
+  ["Basic function-expressions are available: f(d',x)=d'*x; f(3, 5)", ['15']],
+  ['> Also, previously defined functions and variables can be shared, e.g. f(2, x) / .33', []],
+  ['Definitions yield nothing back:', []],
+  ["  a' = 1.2;", []],
+  ["  b' = 3.4;", []],
+  ["  c' = a' * b' / 2;", []],
+  ["Number-less units are not evaluated: c'.", []],
+  ["Numbers along with units are evaluated: 1c', -0.2a' or 2-b'.", ['2.04', '-0.24', '-1.4']],
+  ['# Units', []],
+  ['Some values are already units, like 1cm - 35mm.', ['-2.5 cm']],
+];
+
+const calc = new Solvente();
+
+describe('DSL', () => {
+  cases.forEach(test => {
+    it(test[0], () => {
+      deepEqual(calc.resolve(test[0]).results.map(x => x.format), test[1]);
+    });
+  });
+});
---
+++ tests/sandbox.test.js
@@ -0,0 +1,56 @@
+import Solvente from '../src/lib';
+import { deepEqual } from 'assert';
+
+const cases = [
+  ['2 cm / 35mm', ['0.57 cm']],
+  ['2 cm of 3 inch', ['0.79 in']],
+  ['365 days / 2years', ['0.5 days']],
+  ['2TB + 25GB', ['2.024 TB']],
+  ['3GB / 1MB', ['3,072 GB']],
+  ['2 + 1 inch + 1cm', ['3.39 in']],
+  ['$15,000 MXN / 14 days of **work** 😅😇👋🏿👨‍👨‍👧‍👧', ['1,071.43 MXN']],
+  ['160 pounds as kg', ['72.57 kg']],
+  ['3 weeks as days', ['21 days']],
+  ['1 2 3 / 4 5', ['8.75']],
+  ['1 2 3 4 5', ['15']],
+  ['1 2 3 - 4 5', ['-3']],
+  ['3in / 2 cm', ['3.81 in']],
+  ['1+2-3+4/2+7/-.12', ['-56.33']],
+  ['(1+2) - (3 + 4/2) + (7/-.12)', ['-60.33']],
+  ['(1 + 2) - (3 + (4 / 2)) + (7 / -.12)', ['-60.33']],
+  ['30 + 20%', ['36']],
+  ['100_000', ['100,000']],
+  ['1ft - .3ft', ['0.7 ft']],
+  ['2 cm in 3 in', ['0.79 in']],
+  ['10 inches in cm', ['25.4 cm']],
+  ['450 km in miles', ['279.62 mi']],
+  ['in cm 2+3', ['5 cm']],
+  ['in cm, 2+3', ['5 cm']],
+  ['3 days in 4 years', ['0.0082 year']],
+  ['2+3 as cm', ['5 cm']],
+  ['2+3, as cm', ['5 cm']],
+  ['12 from 1987', ['0.6%']],
+  ['-1 week as 1990', ['Thu Jul 26 1990 00:00:00']],
+  ['1 month from today', ['Sun Sep 01 2019 10:30:00']],
+  ['3:35 am + 9 hours 20 minutes', ['Fri Aug 02 2019 12:55:00']],
+  // ['Yesterday + 3 weeks 2 days', ['Sat Aug 24 2019 21:28:32']],
+  // ['Now at 6:00 pm - Apr 15 2019', ['Fri Aug 02 2019 21:28:32']],
+  // ['Now at 6:00 pm - 3 days 15 min', ['Fri Aug 02 2019 21:28:33']],
+  // ['Now at 6:00 pm - 3 days + 15 min', ['Fri Aug 02 2019 21:28:33']],
+  // ['now as 10 of Jun from 1987', ['Sat Aug 10 2019 21:28:33']],
+  ['Jun 10, 1987 - 1 week', ['Wed Jun 03 1987 00:00:00']],
+  ['Jun 10 of 1987', ['Wed Jun 10 1987 00:00:00']],
+  ['Jun 10 1987', ['Wed Jun 10 1987 00:00:00']],
+  ['Jun 10', ['Mon Jun 10 2019 00:00:00']],
+  ['Jun 1', ['Sat Jun 01 2019 00:00:00']],
+];
+
+const calc = new Solvente();
+
+describe('Sandbox', () => {
+  cases.forEach(test => {
+    it(test[0], () => {
+      deepEqual(calc.resolve(test[0]).results.map(x => x.format), test[1]);
+    });
+  });
+});
