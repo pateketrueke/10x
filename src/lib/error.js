3d34ef4e3e5d5a3d0d0b037c9b654a7f1153be4e
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-08-17 00:40:02 -0500

    OK

--- src/lib/error.js
+++ src/lib/error.js
@@ -14,16 +14,21 @@ export default class ParseError extends Error {
     this.expr = expr;
   }
 
-  static build(e, code) {
+  static build(e, code, lines = 2, source = null) {
     const [[x1, y1], [x2, y2]] = e.ctx.cur._offset;
 
-    e.stack = `${e.message} (at line ${x1 + 1}:${y1 + 1})\n${code.split('\n').map((x, i) => {
-      if (i === x1) {
-        return ` ${pad(i + 1, 3)} | ${x}\n${repeat('-',y1 + 8)}${repeat('^', y2)}^`;
+    e.stack = `${e.message}\n  at ${source || 'line '}${x1 + 1}:${y1 + 1}\n\n${code.split('\n').reduce((prev, cur, i) => {
+
+      if (i >= x1 - lines && i <= x2 + lines) {
+        if (i === x1) {
+          prev.push(` ${pad(i + 1, 5)} | ${cur}\n${repeat('-', y1 + 10)}${repeat('^', y2 - y1)}^`);
+        } else {
+          prev.push(` ${pad(i + 1, 5)} | ${cur}`);
+        }
       }
 
-      return ` ${pad(i + 1, 3)} | ${x}`;
-    }).join('\n')}`;
+      return prev;
+    }, []).join('\n')}\n`;
 
     return e;
   }
