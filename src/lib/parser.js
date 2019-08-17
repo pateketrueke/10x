59e3f7558f0081b844f53b122c24908aa3cfe345
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-08-17 16:08:35 -0500

    Cleanup

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -51,7 +51,7 @@ export const isChar = a => /^[a-zA-Z]+/.test(a);
 
 export const isFmt = x => /^["`_*~]$/.test(x);
 export const isNth = x => /^\d+(?:t[hy]|[rn]d)$/.test(x);
-export const isAny = (x, a = '') => /^[^\s\w\d_*~$€£¢%({[~<!>\]})"`|:;_,.+=*/-]$/.test(x) || a.includes(x);
+export const isAny = (x, a = '') => a.includes(x) || /^[^\s\w\d_*~$€£¢%({[~<!>\]})"`|:;_,.+=*/-]$/.test(x);
 export const isInt = x => typeof x === 'number' || /^-?(?!0)\d+(\.\d+)?$/.test(x);
 export const isNum = x => /^-?[$€£¢]?(?:\.\d+|\d+(?:[_,.]\d+)*)%?/.test(x);
 export const isExpr = x => /^(?:from|and|f?or|to|of|a[ts]|i[ns])$/i.test(x);
