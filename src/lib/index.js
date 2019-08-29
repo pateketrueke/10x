e06cef1fe764e80f8f48fdf00bdc3305576798cd
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-08-29 08:55:50 -0500

    OK

--- src/lib/index.js
+++ src/lib/index.js
@@ -96,7 +96,7 @@ export default class Solv {
 
     if (token[0] === 'number') {
       token[1] = toNumber(token[1]);
-    } else {
+    } else if (typeof token[1] === 'object') {
       token[1] = toPlain(token[1]);
     }
 
