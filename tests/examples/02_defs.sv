f0fc85136ecdb0f5a054b677c60496fc190b7f34
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-09-07 03:01:31 -0500

    WIP

--- tests/examples/02_defs.sv
+++ tests/examples/02_defs.sv
@@ -3,4 +3,42 @@
 Local functions can be achieved too:
 
 sum(a, b)=a + b;
-sum(1, 2)
+sum(1, 2);
+
+# Logic handling
+
+Only three kind of logical support is implemented.
+
+## Matching
+
+Use :if, :else, :not, :unless and :otherwise to do comparisons.
+
+:if (< 1 2) "lower" :else "upper";
+
+:unless (:false) "Completely true";
+
+:not (:true) -1 :otherwise "Falsey";
+
+Multiple matches are allowed through :match, e.g.
+
+testMatch(value)=
+  :match
+    (== value :true) "OK",
+    (== value :false) "NOPE"
+  :otherwise "UNKNOWN";
+
+testMatch("String"),
+testMatch(:false),
+testMatch(:true);
+
+## Loops
+
+## Errors
+
+:try sum() :catch -1;
+
+:try undef();
+
+:catch undef();
+
+:foo (:bar);
