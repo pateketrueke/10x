b13ef33e3e60d79b7f1c8693566fb9ee88932b60
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-09-08 21:12:00 -0500

    Cleanup

--- tests/examples/00_basics.sv
+++ tests/examples/00_basics.sv
@@ -4,6 +4,15 @@ All numeric values are recognized, e.g. -.6, 42
 
 Fractions are supported too, e.g. 1.5 as frac
 
+# Strings
+
+Double quoted strings are supported aswell,
+sigle quotes are not considered for this purpose.
+
+Interpolation is also possible, e.g. "Test: #{1, 2, 3} 😁"
+
+> Notice all expressiones are evaluated and concatenated in place.
+
 # Units
 
 Common units can be used after numbers, e.g. 3 inches, 5mm
@@ -12,7 +21,7 @@ Converting between units is also possible: 3 inches as cm
 
 Define and use your own units: x=1.2; 3x
 
-Scope is also possible within parentheses:
+Scope is created within parentheses:
 
 (x=1.5; 3x)
 2x
